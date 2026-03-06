da3e0c96314d829cb124a830c50def9a7a2a2d18
Olawoore Damilola James <58667901+dammycute@users.noreply.github.com>
2026-03-06 12:38:03 +0100

    fix: fix the broken images, border radius and the spaces between sections

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,16 @@
 const nextConfig = {
   /* config options here */
   reactCompiler: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'rfvqwaewwjqssds1.public.blob.vercel-storage.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
