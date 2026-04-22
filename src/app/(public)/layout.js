3c5eac0680beb9db58d07e94d35124d32410acbe
Olawoore Damilola James <58667901+dammycute@users.noreply.github.com>
2026-04-22 16:54:06 +0100

    feat: add  email collection popup scripts

--- src/app/(public)/layout.js
+++ src/app/(public)/layout.js
@@ -1,9 +1,16 @@
+import Script from "next/script";
 import Navbar from "@/components/Navbar/Navbar";
 import Footer from "@/components/Footer/Footer";
 
 export default function PublicLayout({ children }) {
     return (
         <>
+            <Script
+                src="//web.webformscr.com/apps/fc3/build/loader.js"
+                async
+                strategy="afterInteractive"
+                sp-form-id="85bcd31aea8e1c22649b5573b153935768abdb4009ed594b453dfa8f4009ed594b453dfa8f4009e62a"
+            />
             <Navbar />
             {children}
             <Footer />
